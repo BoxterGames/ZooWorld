f651a3549d306564e298e4d274fc036fa982fc6e
roman-karimov <roman.karimov@say.games>
2025-06-08 11:48:18 +0400

    Initial

---
+++ Assets/Scripts/Controllers/FoodChainController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodChainController
+{
+    public void CollideAnimals(int idFirst, int idSecond)
+    {
+        
+    } 
+}
